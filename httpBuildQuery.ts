40576cf6783a39b87711fc5ed404ce71e1b61b59
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-02-17 15:45:12 -0300

    add fn httpQueryToObject

--- httpBuildQuery.ts
+++ httpBuildQuery.ts
@@ -1,16 +1,26 @@
-
-
 /**
  * 
  *
- * @param {*} objParams
- * @returns
+ * @param {object} objParams
+ * @returns {string}
  */
-const httpBuildQuery = (objParams:any, encode:boolean = true)=>{
+export const buildHttpQuery = (objParams:any, encode:boolean = true)=>{
     return Object.keys(objParams).map(key => {
         if(encode) return `${encodeURIComponent(key)}=${encodeURIComponent(objParams[key])}`;
         return `${key}=${objParams[key]}`;
     }).join('&');
-}
+};
 
-export default httpBuildQuery;
+
+/**
+ * Converte http query string in object
+ * @param {string} query   @examples "a=true&b=false&c=[1,2,4,88]"
+ * @returns {object}
+ */
+export const httpQueryToObject = (query:string) => {
+    return query.split('&').reduce((accumulator, valueCurrent)=>{
+        const [key,value] = valueCurrent.split('=');
+        accumulator[key] = value;
+        return accumulator;
+    }, {});
+};
