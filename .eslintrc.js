2633d28b1932fac1cb3ba6df4f417ef15da8cc92
Luiz Carlos W Santos <luizcarloswagner@gmail.com>
2020-06-08 10:46:34 -0300

    alera styleguide eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // root: true,
+  env: {
+    browser: true,
+    es6: true,
+    // node: true,
+    jest: true,
+  },
+  extends: ['standard', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+  settings: {},
+};
