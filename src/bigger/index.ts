8a5c4ae3b1b8636a341c2814a6ada526cf54ab33
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-07-21 12:32:57 -0300

    altera estilo do argumento

--- src/bigger/index.ts
+++ src/bigger/index.ts
@@ -1,4 +1,4 @@
 // Retorna o  maior valor do Array
-const getBigger = (needle: number[]) => Math.max(...needle);
+const getBigger = (...needle: number[]) => Math.max(...needle);
 
 export default getBigger;
