a1b1e5fedf80cc7f2371227949ed2890f827bf42
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-11-06 13:53:13 -0300

    chore: altera para melhor entendimento

--- src/uniqueId/index.ts
+++ src/uniqueId/index.ts
@@ -1,9 +1,10 @@
 const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const lastIndexFromCharacters = characters.length - 1;
 
 const uniqueId = (maxCharacters = 6, namespace = '') => {
   let result = namespace;
   while (maxCharacters--) {
-    const index = Math.round(Math.random() * (--characters.length));
+    const index = Math.round(Math.random() * lastIndexFromCharacters);
     result += characters[index];
   }
   return result;
