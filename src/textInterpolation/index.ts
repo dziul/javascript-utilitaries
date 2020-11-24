86d68494a8a415000b908ef02030cb008089336f
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-11-24 18:53:52 -0300

    fix: add flag S

--- src/textInterpolation/index.ts
+++ src/textInterpolation/index.ts
@@ -4,7 +4,7 @@
 const textInterpolation = (text: string, variablesObject: {[k:string]:any}, delimiter: [string,string] = ['{{', '}}']) => {
   let textInterpolated = text;
   Object.keys(variablesObject).forEach((key) => {
-    const regex = new RegExp(`${delimiter[0]}${key}${delimiter[1]}`, 'g');
+    const regex = new RegExp(`${delimiter[0]}${key}${delimiter[1]}`, 'gs');
     textInterpolated = textInterpolated.replace(regex, variablesObject[key]);
   });
   return textInterpolated;
