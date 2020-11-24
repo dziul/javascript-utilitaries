dddd0500a9c0115e4dfa34dc0c4dfc52d3fee410
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-11-24 16:11:46 -0300

    refactor: altera tipagem

--- src/textInterpolation/index.ts
+++ src/textInterpolation/index.ts
@@ -1,7 +1,7 @@
 /**
  * Interpolação de texto
  */
-const textInterpolation = (text: string, variablesObject: {[k:string]:any}, delimiter: string[] = ['{{', '}}']) => {
+const textInterpolation = (text: string, variablesObject: {[k:string]:any}, delimiter: [string,string] = ['{{', '}}']) => {
   let textInterpolated = '';
   Object.keys(variablesObject).forEach((key) => {
     const regex = new RegExp(`${delimiter[0]}${key}${delimiter[1]}`, 'g');
