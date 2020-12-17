5be3b76f14cc9fe245d4b69e65782e27ac3d4bf0
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-12-17 10:18:37 -0300

    feat: add containsKeyInObject

--- src/objectUtil/index.ts
+++ src/objectUtil/index.ts
@@ -1,12 +1,14 @@
-const isEquals = (...objs: {[k:string]:any}[]) => {
+export const isEquals = (...objs: {[k:string]:any}[]) => {
     const reference = JSON.stringify(objs.shift());
     return objs.every(obj => {
       return reference === JSON.stringify(obj);
     });
   }
 
-const isObject = (needle: any) => needle !== null && typeof needle === 'object';
+export const isObject = (needle: any) => needle !== null && typeof needle === 'object';
 
-const values = <T extends { [key: string]: any }>(obj: T): ValueOf<T>[] => Object.values(obj);
+export const values = <T extends { [key: string]: any }>(obj: T): ValueOf<T>[] => Object.values(obj);
 
-const keys = <T extends { [key: string]: any }>(obj: T): (keyof T)[] => Object.keys(obj);
+export const keys = <T extends { [key: string]: any }>(obj: T): (keyof T)[] => Object.keys(obj);
+
+export const containsKeyInObject = (obj: {[k:string]:any}, ...keys: string[]) => keys.every(key => obj.hasOwnProperty(key));
