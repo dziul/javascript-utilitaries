b978f99ef536ee85837cbd5d12d08d9b72d31679
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2021-11-23 10:03:42 -0300

    feat: add onlyLetters

--- src/stringNormalize/index.ts
+++ src/stringNormalize/index.ts
@@ -1,6 +1,12 @@
-/**
- * Remove acentos
- */
-const stringNormalize = (text: string): string => text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+const stringNormalize = (text: string): string =>
+  text.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 
-export default stringNormalize;
+export const onlyLetters = (str: string, allowNumber = false) => {
+  const pattern = allowNumber
+    ? /[^\sa-zA-Z0-9\u00C0-\u00FF]+/g
+    : /[^\sa-zA-Z\u00C0-\u00FF]+/g
+
+  return str.replace(pattern, '')
+}
+
+export default stringNormalize
