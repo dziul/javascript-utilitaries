72cbb88f0fc0ac6f8a8b9f81932e7310789af55b
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2022-04-14 00:14:23 -0300

    refactor: mordenize function

--- src/textInterpolation/index.ts
+++ src/textInterpolation/index.ts
@@ -1,13 +1,13 @@
-/**
- * Interpolação de texto
- */
-const textInterpolation = (text: string, variablesObject: {[k:string]:any}, delimiter: [string,string] = ['{{', '}}']) => {
-  let textInterpolated = text;
-  Object.keys(variablesObject).forEach((key) => {
-    const regex = new RegExp(`${delimiter[0]}${key}${delimiter[1]}`, 'gs');
-    textInterpolated = textInterpolated.replace(regex, variablesObject[key]);
-  });
-  return textInterpolated;
+export const simpleTemplaning = (
+    template: string,
+    variablesObject: { [k: string]: string | boolean | number | null | undefined },
+    delimiter: [string, string] = ['{{', '}}']
+) => {
+  return Object.entries(variablesObject).reduce((templateAcc, [key, value]) => {
+      const pattern = new RegExp(`${delimiter[0]}${key}${delimiter[1]}`, 'gs')
+      const replaceValue = value ? String(value) : ''
+      return templateAcc.replace(pattern, replaceValue)
+    }, template)
 };
 
 export default textInterpolation;
