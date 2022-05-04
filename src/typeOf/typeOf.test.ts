7ea7e37f7291d60c81e6b18e9aff895b806e4627
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2022-05-04 10:54:53 -0300

    test: add tests

--- src/typeOf/typeOf.test.ts
+++ src/typeOf/typeOf.test.ts
@@ -1,1 +1,15 @@
+import { typeOf } from '.';
 
+describe('Função typeOf', () => {
+  it('deveria retornar corretamente o tipo do arg passado', () => {
+    expect(typeOf({})).toBe('object');
+    expect(typeOf([])).toBe('array');
+    expect(typeOf(1)).toBe('number');
+    expect(typeOf(1.2)).toBe('number');
+    expect(typeOf(false)).toBe('boolean');
+    expect(typeOf(undefined)).toBe('undefined');
+    expect(typeOf(null)).toBe('null');
+    expect(typeOf(new Date())).toBe('date');
+    expect(typeOf(new Set())).toBe('set');
+  });
+});
