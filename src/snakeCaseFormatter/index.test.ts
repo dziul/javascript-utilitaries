82e22442b9528f91418f4ec279873369c451a05d
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2023-02-17 10:51:54 -0300

    Delete index.test.ts

--- src/snakeCaseFormatter/index.test.ts
+++
@@ -1,8 +0,0 @@
-import snakeCase, { screamingSnakeCaseFormatter } from './index';
-
-it('Should covert "Hello People!" to "hello_people"', () => {
-  expect(snakeCase('Hello Word!')).toEqual('hello_word');
-});
-it('Should covert "Helló Peoplê!" to "HELLO_PEOPLE"', () => {
-  expect(screamingSnakeCaseFormatter('Helló Peoplê!')).toEqual('HELLO_PEOPLE');
-});
