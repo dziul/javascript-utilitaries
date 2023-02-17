c60fbbb0c9394a2f3e36655913844cea865837b5
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2023-02-17 09:36:54 -0300

    refactor(toSnakeCase): melhora lógica

---
+++ src/toSnakeCase/index.ts
@@ -0,0 +1,26 @@
+import normalizeText from '../stringNormalize';
+
+/**
+ * retorna novo texto no formato `snake_case`
+ *
+ * @param text texto que deseja formatar
+ *
+ * @example
+ * ```js
+ * formatSnakeCase("camelCase") // => "camel_case"
+ * formatSnakeCase("pá+_~--tô") // => "pa_to"
+ * formatSnakeCase('{"key":"value"}') // => "key_value"
+ * ```
+ */
+export const toSnakeCase = (text: string) => {
+  return normalizeText(text.trim())
+    .split(/\s+/)
+    .map(partialText => partialText.replace(/\W+/g, '_'))
+    .join('_')
+    .replace(/([a-z])([A-Z])/g, '$1_$2')
+    .replace(/_+/g, '_')
+    .replace(/^_|_$/g, '')
+    .toLocaleLowerCase();
+};
+
+export default snakeCaseFormatter;
